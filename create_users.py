05084186482aa2adb4e7c132d10b511ff017e6a7
Mudasir Rehman <mudasirrehman.dev@gmail.com>
2026-08-14 16:37:15 +0500

    Backend logic Complete

--- create_users.py
+++ create_users.py
@@ -6,23 +6,23 @@ from werkzeug.security import generate_password_hash
 
 with app.app_context():
 
-    # Check if student2 already exists
+    # Check if student3 already exists
     existing_user = User.query.filter_by(
-        username="student2"
+        username="student3"
     ).first()
 
     if existing_user:
-        print("student2 already exists")
+        print("student3 already exists")
 
     else:
 
-        student2 = User(
-            username="student2",
+        student3 = User(
+            username="student3",
             password_hash=generate_password_hash("student123"),
             role="student"
         )
 
-        db.session.add(student2)
+        db.session.add(student3)
         db.session.commit()
 
-        print("student2 created successfully!")
+        print("student3 created successfully!")
