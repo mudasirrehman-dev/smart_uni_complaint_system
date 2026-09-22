c282ff8f2d668bc3fd11bae0bd37f6bf85122c32
Mudasir Rehman <mudasirrehman.dev@gmail.com>
2026-09-22 11:26:54 +0500

    Working prototype before new requirements

--- create_users.py
+++ create_users.py
@@ -6,23 +6,23 @@ from werkzeug.security import generate_password_hash
 
 with app.app_context():
 
-    # Check if student4 already exists
+    # Check if student5 already exists
     existing_user = User.query.filter_by(
-        username="student4"
+        username="student5"
     ).first()
 
     if existing_user:
-        print("student4 already exists")
+        print("student5 already exists")
 
     else:
 
-        student4 = User(
-            username="student4",
+        student5 = User(
+            username="student5",
             password_hash=generate_password_hash("student123"),
             role="student"
         )
 
-        db.session.add(student4)
+        db.session.add(student5)
         db.session.commit()
 
-        print("student4 created successfully!")
+        print("student5 created successfully!")
